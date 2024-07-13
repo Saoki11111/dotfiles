e56aabc8a6ce479360e10becffe875f5427ed9df
Saoki11111 <t.aoki11111@gmail.com>
2024-07-13 21:29:19 +0900

    /で大文字小文字を検索できるように設定

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -2,8 +2,13 @@
 vim.opt.expandtab = true      -- タブ入力を複数の空白入力に置き換える
 vim.opt.tabstop = 2           -- 画面上でタブ文字が占める幅
 vim.opt.softtabstop = 2       -- 連続した空白に対してタブキーやバックスペースキーでカーソルが動く幅
+
 vim.opt.autoindent = true     -- 改行時に前の行のインデントを継続する
+
 vim.opt.shiftwidth = 2        -- smartindentで増減する幅
+
+vim.o.ignorecase = true
+vim.o.smartcase = true
 
 vim.o.scrolloff = 5
 
