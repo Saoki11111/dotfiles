0144fb9a012cc20284aef49ef20379026af44a73
Saoki11111 <t.aoki11111@gmail.com>
2024-07-14 22:38:07 +0900

    file パス修正

--- migrate.sh
+++ migrate.sh
@@ -8,10 +8,10 @@ ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
 
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 
-ln -sf ~/dotfiles/.hammerspoon ~/.hammerspoon
+ln -sf ~/dotfiles/.hammerspoon ~
 
 mkdir -p ~/.config
-ln -sf ~/dotfiles/.config/nvim ~/.config/
+ln -sf ~/dotfiles/.config/nvim ~/.config
 ln -sf ~/dotfiles/.config/starship.toml ~/.config/starship.toml
 
 echo "configuration has been migrated successfully."
